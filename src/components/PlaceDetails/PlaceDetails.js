0c99e34ade781c584bfe7654e25794a8b653c56c
amisha26 <amisha8299@gmail.com>
2021-10-29 09:54:11 +0530

    PlaceDetails.js

---
+++ src/components/PlaceDetails/PlaceDetails.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles.js';
+
+const PlaceDetails = ({ place, selected, refProp }) => {
+  if (selected) refProp?.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  const classes = useStyles();
+
+  return (
+    <Card elevation={6}>
+     
+    </Card>
+  );
+};
+
+export default PlaceDetails;
