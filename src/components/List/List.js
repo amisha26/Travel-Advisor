6f963e2b2f00cf004d4d565fb4bec9bd21b983b1
amisha26 <amisha8299@gmail.com>
2021-10-29 09:42:26 +0530

    List.js formcontrol

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -29,7 +29,16 @@ const List = ({ places, type, setType, rating, setRating, childClicked, isLoadin
               <MenuItem value="attractions">Attractions</MenuItem>
             </Select>
           </FormControl>
-    
+          <FormControl className={classes.formControl}>
+            <InputLabel id="rating">Rating</InputLabel>
+            <Select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
+              <MenuItem value="">All</MenuItem>
+              <MenuItem value="3">Above 3.0</MenuItem>
+              <MenuItem value="4">Above 4.0</MenuItem>
+              <MenuItem value="4.5">Above 4.5</MenuItem>
+            </Select>
+          </FormControl>
+         
         </>
       )}
     </div>
