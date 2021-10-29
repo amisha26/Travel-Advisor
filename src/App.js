0e1c3143869bc9ac2fa8fba69028b1d812e60eab
amisha26 <amisha8299@gmail.com>
2021-10-29 09:58:33 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
 
 const App = () => {
-  return (
-    <div>
-      Travel
-    </div>
-  )
-}
+  
+ 
 
-export default App
+ 
+  
+
+
+  return (
+    <>
+      <CssBaseline />
+      
+    </>
+  );
+};
+
+export default App;
