184bc76c0ee4563e19dd27fb5d40608a717579a1
amisha26 <amisha8299@gmail.com>
2021-10-29 09:56:26 +0530

    PlaceDetails.js

--- src/components/PlaceDetails/PlaceDetails.js
+++ src/components/PlaceDetails/PlaceDetails.js
@@ -41,7 +41,19 @@ const PlaceDetails = ({ place, selected, refProp }) => {
             <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
           </Box>
         ))}
-       
+        {place?.cuisine?.map(({ name }) => (
+          <Chip key={name} size="small" label={name} className={classes.chip} />
+        ))}
+        {place.address && (
+          <Typography gutterBottom variant="body2" color="textSecondary" className={classes.subtitle}>
+            <LocationOnIcon />{place.address}
+          </Typography>
+        )}
+        {place.phone && (
+          <Typography variant="body2" color="textSecondary" className={classes.spacing}>
+            <PhoneIcon /> {place.phone}
+          </Typography>
+        )}
       </CardContent>
       <CardActions>
         <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')}>
