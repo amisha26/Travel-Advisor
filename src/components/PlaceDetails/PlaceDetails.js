1dc59616a68621b0a0f8f9c22d515898814bb976
amisha26 <amisha8299@gmail.com>
2021-10-29 09:55:22 +0530

    PlaceDetails.js

--- src/components/PlaceDetails/PlaceDetails.js
+++ src/components/PlaceDetails/PlaceDetails.js
@@ -12,7 +12,17 @@ const PlaceDetails = ({ place, selected, refProp }) => {
 
   return (
     <Card elevation={6}>
-     
+      <CardMedia
+        style={{ height: 350 }}
+        image={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+        title={place.name}
+      />
+      <CardContent>
+        
+      </CardContent>
+      <CardActions>
+      
+      </CardActions>
     </Card>
   );
 };
